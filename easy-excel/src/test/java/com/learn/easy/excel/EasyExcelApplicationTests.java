ad29e79335afc98d3d8e8ccf2a416049148ff839
CaoLongHui <1812806483@qq.com>
2024-09-05 12:44:09 -0500

    first commit

---
+++ easy-excel/src/test/java/com/learn/easy/excel/EasyExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.easy.excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
